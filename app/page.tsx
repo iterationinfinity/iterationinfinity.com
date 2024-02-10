a4d97d711830d8fb28be6f3515488e8c4036a277
cykreet <cykreet@gmail.com>
2024-02-10 07:46:56 +0200

    chore: replace email with socials

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,37 @@
 import Header from "./components/header";
+import { Instagram, Twitter } from "feather-icons-react";
 
 export default function Page() {
 	return (
 		<div className="container mx-auto">
 			<Header />
-			<div className="mt-28 m-2 text-center sm:text-left">
-				<div className="uppercase font-black font-head text-8xl">
+			<div className="mt-28 m-2 text-left p-6">
+				<div className="uppercase font-black text-8xl">
 					<h1 className="title">Arriving</h1>
 					<h1>
 						<span>Soon.</span>
 					</h1>
 				</div>
 				<div className="flex text-xl mt-6">
-					<div className="float-right sm:pl-80 leading-none space-y-4 opacity-80">
+					<div className="float-right sm:pl-80 leading-none space-y-4">
 						<p>please be patient, while we work on building a space to showcase our passion and work.</p>
 						<p className="font-barcode text-6xl">iterate</p>
 					</div>
 				</div>
 			</div>
 			<div className="text-center mt-20">
-				<p className="opacity-50 text-xl">in the meantime, let's talk</p>
-				<a href="mailto:mail@iterationinfinity.com">mail@iterationinfinity.com</a>
+				<p className="text-xl mb-4">in the meantime, let's talk</p>
+				<div className="flex flex-row gap-4 place-content-center">
+					<a href="https://instagram.com/iterationinfinity/" className="mb-4">
+						<Instagram />
+					</a>
+					<a href="https://twitter.com/iterateinfinity" className="mb-4">
+						<Twitter />
+					</a>
+				</div>
+				{/* <a href="mailto:mail@iterationinfinity.com" className="mb-4">
+					mail@iterationinfinity.com
+				</a> */}
 			</div>
 		</div>
 	);
